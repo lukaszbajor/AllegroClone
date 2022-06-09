5b7d7c6528b109ccfb5f050253e6a9015ebc9807
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2022-06-09 17:14:51 +0200

    good display main sections with responsive is done

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ const showContent = () => {
   arrowDown.classList.toggle("category__boxOne__icon--iconUp");
 };
 arrowDown.addEventListener("click", showContent);
+
+//show offer and cover other main elements
+
+const offersSection = document.querySelector(".offers");
+const allCategories = document.querySelectorAll(
+  ".category__boxSecond__list__item"
+);
+const firstMainBlock = document.querySelector(".blockFirst");
+const secondMainBlock = document.querySelector(".blockSecond");
+
+allCategories.forEach((item) => {
+  item.addEventListener("click", () => {
+    offersSection.classList.add("offers--showOffers");
+    firstMainBlock.classList.add("blockFirst--coverContent");
+    secondMainBlock.classList.add("blockSecond--coverContent");
+  });
+});
